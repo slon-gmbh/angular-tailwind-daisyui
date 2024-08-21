a256f3a05fc851f71934f19cbfabbc14f6345967
OleWahn <olerummel@web.de>
2024-08-21 14:44:28 +0200

    add postinstall

---
+++ postinstall.js
@@ -0,0 +1,21 @@
+const fs = require('fs');
+const path = require('path');
+
+// Path to the template folder in your npm package
+const templateDir = path.join(__dirname, 'template');
+
+// Files to copy
+const filesToCopy = ['postcss.config.js', 'tailwind.config.js'];
+
+filesToCopy.forEach(file => {
+    const src = path.join(templateDir, file);
+    const dest = path.join(process.cwd(), file);
+
+    // Copy file if it doesn't already exist
+    if (!fs.existsSync(dest)) {
+        fs.copyFileSync(src, dest);
+        console.log(`Created ${file} in the project root`);
+    } else {
+        console.log(`${file} already exists in the project root`);
+    }
+});
